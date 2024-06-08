cdce90118adbb47b15fc2c2e3177c79555be51bc
Bruno Silva <bruno_gsilva@outlook.com>
2024-06-08 10:40:46 -0300

    SimpleAtari curriculum

---
+++ project/curriculums/atari.py
@@ -0,0 +1,32 @@
+import typing
+import numpy as np
+import tella
+
+
+class SimpleAtariSequenceCurriculum(tella.curriculum.InterleavedEvalCurriculum):
+    TASKS = ["RoadRunner", "Boxing", "Jamesbond"]
+
+    def eval_block(self) -> tella.curriculum.EvalBlock:
+        rng = np.random.default_rng(self.eval_rng_seed)
+        return tella.curriculum.simple_eval_block(
+            tella.curriculum.TaskVariant(
+                tella._curriculums.atari.environments.ATARI_TASKS[task_label],
+                task_label=task_label,
+                num_episodes=2,
+                rng_seed=rng.bit_generator.random_raw(),
+            )
+            for task_label in self.TASKS
+        )
+
+    def learn_blocks(self) -> typing.Iterable[tella.curriculum.LearnBlock]:
+        for task_label in self.TASKS:
+            yield tella.curriculum.simple_learn_block(
+                [
+                    tella.curriculum.TaskVariant(
+                        tella._curriculums.atari.environments.ATARI_TASKS[task_label],
+                        task_label=task_label,
+                        num_steps=5,
+                        rng_seed=self.rng.bit_generator.random_raw(),
+                    )
+                ] * 2
+            )
---
+++ project/curriculums/test.py
@@ -0,0 +1,97 @@
+
+import logging
+import typing
+
+import gym
+import tella
+from atari import SimpleAtariSequenceCurriculum
+
+logger = logging.getLogger("Example Logging Agent")
+
+
+class LoggingAgent(tella.ContinualRLAgent):
+    def __init__(
+        self,
+        rng_seed: int,
+        observation_space: gym.Space,
+        action_space: gym.Space,
+        num_envs: int,
+        config_file: typing.Optional[str] = None,
+    ) -> None:
+        super().__init__(
+            rng_seed, observation_space, action_space, num_envs, config_file
+        )
+        logger.info(
+            f"Constructed with observation_space={observation_space} "
+            f"action_space={action_space} num_envs={num_envs}"
+        )
+
+    def block_start(self, is_learning_allowed: bool) -> None:
+        super().block_start(is_learning_allowed)
+        if is_learning_allowed:
+            logger.info("About to start a new learning block")
+        else:
+            logger.info("About to start a new evaluation block")
+
+    def task_start(
+        self,
+        task_name: typing.Optional[str],
+    ) -> None:
+        logger.info(
+            f"\tAbout to start interacting with a new task. task_name={task_name}"
+        )
+
+    def task_variant_start(
+        self,
+        task_name: typing.Optional[str],
+        variant_name: typing.Optional[str],
+    ) -> None:
+        logger.info(
+            f"\tAbout to start interacting with a new task variant. "
+            f"task_name={task_name} variant_name={variant_name}"
+        )
+
+    def choose_actions(
+        self, observations: typing.List[typing.Optional[tella.Observation]]
+    ) -> typing.List[typing.Optional[tella.Action]]:
+        logger.debug(f"\t\t\tReturn {len(observations)} random actions")
+        return [
+            None if obs is None else self.action_space.sample() for obs in observations
+        ]
+
+    def receive_transitions(
+        self, transitions: typing.List[typing.Optional[tella.Transition]]
+    ) -> None:
+        for transition in transitions:
+            if transition is not None:
+                obs, action, reward, done, next_obs = transition
+                logger.debug(f"\t\t\tReceived transition done={done}")
+
+    def task_end(
+        self,
+        task_name: typing.Optional[str],
+    ) -> None:
+        logger.info(f"\tDone interacting with task. task_name={task_name}")
+
+    def task_variant_end(
+        self,
+        task_name: typing.Optional[str],
+        variant_name: typing.Optional[str],
+    ) -> None:
+        logger.info(
+            f"\tDone interacting with task variant. "
+            f"task_name={task_name} variant_name={variant_name}"
+        )
+
+    def block_end(self, is_learning_allowed: bool) -> None:
+        if is_learning_allowed:
+            logger.info("Done with learning block")
+        else:
+            logger.info("Done with evaluation block")
+
+
+if __name__ == "__main__":
+    tella.curriculum.curriculum_registry['SimpleAtari'] = SimpleAtariSequenceCurriculum
+    logging.basicConfig(level=logging.INFO)
+    tella.rl_cli(LoggingAgent)
+
